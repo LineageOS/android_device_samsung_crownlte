d5109f72c84e2420f389090d5953b50d4c1a8b0e
synt4x93 <synt4x.93@gmail.com>
2019-06-23 01:33:30 +0000

    crownlte: Add fingerprint override
    Change-Id: Ia32b23c4f71f13be10811c3c95e753abca087990

--- lineage_crownlte.mk
+++ lineage_crownlte.mk
@@ -7,3 +7,8 @@ $(call inherit-product, device/samsung/crownlte/full_crownlte.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_crownlte
+
+BUILD_FINGERPRINT := samsung/crownltexx/crownlte:10/QP1A.190711.020/N960FXXU5DTCA:user/release-keys
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+        PRIVATE_BUILD_DESC="crownltexx-user 10 QP1A.190711.020 N960FXXU5DTCA release-keys"
