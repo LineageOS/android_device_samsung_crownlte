9363c25d51a10bc21f50472bdfb51be260a1347d
Shenhua <33694493+Shenhua99@users.noreply.github.com>
2020-02-04 20:58:32 +0100

    crownlte: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Change-Id: I599201740335e940370dcda555531e5c5e05aadc

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,3 +3,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_crownlte.mk
+
+COMMON_LUNCH_CHOICES := \
+     lineage_crownlte-userdebug \
+     lineage_crownlte-eng \
+     lineage_crownlte-user
