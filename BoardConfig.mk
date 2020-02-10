eaa5ae90f37857010259b6fbabe5f623568420cb
Erfan Abdi <erfangplus@gmail.com>
2020-02-10 13:04:40 +0330

    crownlte: Cleanup tree
    * Based on https://github.com/exynos8895/android_device_samsung_dreamlte
    
    Change-Id: I1110986e8a9bcb0b7bba54ebef4daf200d05bace

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,8 +9,5 @@ TARGET_OTA_ASSERT_DEVICE := crownltexx,crownlteks,crownlte
 # Kernel
 TARGET_KERNEL_CONFIG := exynos9810-crownlte_defconfig
 
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
-
 # Inherit common board flags
 include device/samsung/universal9810-common/BoardConfigCommon.mk
--- lineage_crownlte.mk
+++ lineage_crownlte.mk
@@ -1,14 +1,29 @@
 # Copyright (C) 2018 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 
-$(call inherit-product, device/samsung/crownlte/full_crownlte.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/crownlte/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := crownlte
 PRODUCT_NAME := lineage_crownlte
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-N960F
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 BUILD_FINGERPRINT := samsung/crownltexx/crownlte:10/QP1A.190711.020/N960FXXU5DTCA:user/release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
+        PRODUCT_NAME=crownltexx \
         PRIVATE_BUILD_DESC="crownltexx-user 10 QP1A.190711.020 N960FXXU5DTCA release-keys"
